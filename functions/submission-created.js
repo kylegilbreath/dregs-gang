0af4ad07c91c97d5d06820784ff2d22bdd6ce109
Kyle Gilbreath <kyle.gilbreath@sendgrid.com>
2019-08-09 17:15:44 -0600

    trying new random array

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -9,12 +9,16 @@ const {
 } = process.env;
 const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
-var myArray = [
-  "'https://demo.twilio.com/owl.png'",
-  "'https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg'"
-];
+function random_item(items)
+{
+  return items[Math.floor(Math.random()*items.length)];    
+}
 
-var randomPic = myArray[Math.floor(Math.random()*myArray.length)];
+var items = [
+  "https://demo.twilio.com/owl.png",
+  "https://c1.staticflickr.com/3/2899/14341091933_1e92e62d12_b.jpg"
+];
+console.log(random_item(items));
 
 exports.handler = function(event, context, callback) {
   Promise.all(
@@ -23,7 +27,7 @@ exports.handler = function(event, context, callback) {
         from: BOT_NUMBER,
         to: num,
         body: "test",
-        mediaUrl: [randomPic]
+        mediaUrl: [random_item(items)]
         // mediaUrl: ['https://demo.twilio.com/owl.png']
       });
     })
