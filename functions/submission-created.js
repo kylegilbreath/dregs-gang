28f9fbfb60da031fa810568fca309dfdc16c1681
Kyle Gilbreath <kyle.gilbreath@sendgrid.com>
2019-09-15 20:48:16 -0600

    Adding timbe pic and text

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -17,9 +17,12 @@ function random_item(items)
 var items = [
   "https://i.pinimg.com/736x/3c/f7/9c/3cf79cd78bb2e87538b793245f88c6ba.jpg",
   "https://i.redd.it/cwxogh8lz8s01.jpg",
-  "https://static.fjcdn.com/pictures/Mushroom_73dc01_5423255.jpg",
   "https://mycotopia.net/uploads/monthly_01_2008/post-13626-13818573074.jpg",
-  "http://www.naturefoto2000.com/image/Phallus%20impudicus%20IMG_0021xnM.jpg"
+  "http://www.naturefoto2000.com/image/Phallus%20impudicus%20IMG_0021xnM.jpg",
+  "https://static.fjcdn.com/pictures/Mushroom_73dc01_5423255.jpg",
+  "https://i.imgur.com/ajHd330.jpg",
+  "https://i.imgur.com/R1AOUyE.jpg",
+  "https://freerangestock.com/sample/81509/penis-mushroom-and-mulch.jpg"
 ];
 console.log(random_item(items));
 
@@ -29,7 +32,7 @@ exports.handler = function(event, context, callback) {
       return client.messages.create({
         from: BOT_NUMBER,
         to: num,
-        body: "Keep your head up",
+        body: "Keep your head up, buddy!",
         mediaUrl: [random_item(items)]
       });
     })
