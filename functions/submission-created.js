f9b9d2c2adcea01c8b5538051e9d3b3d88bb4243
Kyle Gilbreath <kyle.gilbreath@sendgrid.com>
2019-12-22 21:08:43 -0700

    Adding Brenner Franz pics

--- functions/submission-created.js
+++ functions/submission-created.js
@@ -15,28 +15,25 @@ function random_item(items)
 }
 
 var items = [
-  "https://253qv1sx4ey389p9wtpp9sj0-wpengine.netdna-ssl.com/wp-content/uploads/2019/09/Hard_Seltzer_Comp_1920x1280-700x461.jpg",
-  "https://253qv1sx4ey389p9wtpp9sj0-wpengine.netdna-ssl.com/wp-content/uploads/2019/09/Bon_Viv_Credit_Candace_Hampton_1600wide.jpg",
-  "https://cached.imagescaler.hbpl.co.uk/resize/scaleWidth/815/cached.offlinehbpl.hbpl.co.uk/news/ORP/whiteclaw8-15-2019-20190819070537254.jpg",
-  "https://media3.s-nbcnews.com/i/newscms/2019_34/1474068/natalie-morales-spiked-seltzer-willie-geist_a6cccb22c29ad1fafb9106b0a367705b.jpg",
-  "https://image.cnbcfm.com/api/v1/image/106245361-1573743748239budlightseltzer.jpeg?v=1573743755&w=678&h=381",
-  "https://assets3.thrillist.com/v1/image/2821567/size/tmg-article_default_mobile_2x.jpg",
-  "https://assets.rbl.ms/20553644/210x.jpg",
-  "https://www.bonnaroo.com/wp-www-bonnaroo-com/wp/wp-content/uploads/2016/10/2-156005c5.jpg",
-  "https://media0.giphy.com/media/VcwV9B5KCs0JsfNRn2/giphy.gif?cid=790b761197b5bd43cf2f9fc1cf0ebf329e980a5943d472b2&rid=giphy.gif",
-  "https://media0.giphy.com/media/Y42RNDQsGYlzAb7G2K/giphy.gif?cid=790b76119171ca5eb681bd7ad6c0320efa984b615e5606dd&rid=giphy.gif",
-  "https://media1.giphy.com/media/MCR8oAVTma7jiwshAz/200.webp?cid=790b761192b7f809b8ba674ee9bd93b2122c58bf43bf40b8&rid=200.webp",
-  "https://media0.giphy.com/media/RlkHvILb2jDyP18kNz/200.webp?cid=790b761192b7f809b8ba674ee9bd93b2122c58bf43bf40b8&rid=200.webp",
-  "https://s3-prod.adage.com/s3fs-public/styles/width_1024/public/6canlineup20180413.jpg",
-  "https://hips.hearstapps.com/vader-prod.s3.amazonaws.com/1559142110-truly-hard-seltzer-1559142094.jpg?crop=1xw:1xh;center,top&resize=480:*",
-  "https://thenypost.files.wordpress.com/2019/08/spiked-seltzer-main-1a.jpg?quality=90&strip=all&w=1200",
-  "https://images.agoramedia.com/everydayhealth/gcms/Is-Whiteclaw-Unhealthy-722x406.jpg?width=546",
-  "https://untappd.akamaized.net/site/beer_logos_hd/beer-3344701_ae468_hd.jpeg",
-  "https://icdn2.themanual.com/image/themanual/mighty-swell-v2.jpg",
-  "https://images1.westword.com/imager/u/blog/11477445/dbc.hard.seltzer.jpg",
-  "https://trulyhardseltzer.com//app_media/TrulyRedux/Social/LiveTrulyPanel_Summer19.file",
-  "https://pbs.twimg.com/media/EFfiz1PWkAkM5z9.jpg",
-  "https://images.squarespace-cdn.com/content/v1/58c1b78586e6c0616c2cbed2/1555591137063-UJFC1NT9Y24KAVENZ75C/ke17ZwdGBToddI8pDm48kHEFWEt6g4d7FcdTSvFDJS57gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0g9xiYCO_4ze-uEG5pWlE5NtjNWuFqjBEdH6rfmVTa6f_PTvzwhkr3_2kdhvhcLbow/IMG_0584+-+Copy.JPG?format=1500w"
+  "https://i.chzbgr.com/full/7340101376/h69E2EF3F/too-easy",
+  "https://66.media.tumblr.com/5a1f41add6a51bac796ba0e44e016919/tumblr_pt3o2jE8z01qjrmjb_400.jpg",
+  "https://pics.awwmemes.com/he-didnt-even-slap-the-bag-imao-at-a-tailgate-37784843.png",
+  "https://i.pinimg.com/originals/c2/10/92/c210927064764b9c61ae960f247fc94a.jpg",
+  "https://1.bp.blogspot.com/-oDQAfgnhytU/TazRwNylHLI/AAAAAAAAAII/5XNegUU-sZA/s1600/franzia+2.jpg",
+  "https://i.chzbgr.com/original/7047588608/hD440ADE1/not-even-once-wine-franzia-7047588608",
+  "https://cdn.totalfratmove.com/wp-content/uploads/2013/09/78805a221a988e79ef3f42d7c5bfd418620883367-600x800.jpg",
+  "https://dailybruin.com/images/2016/11/web.blogging.slapbag-640x426.jpg",
+  "http://m.quickmeme.com/img/0b/0b444c4e037cff189f53b28ba6698bef8002d2af53d4b412a371ef41d0a80abb.jpg",
+  "https://pics.me.me/death-to-zinfandels-franzia-dont-act-like-this-isnt-funny-13919566.png",
+  "https://i5.walmartimages.com/asr/d560fc9c-8c77-49a2-9c1f-33cfe668884b_1.c3a00bf36cc92c480d6b94ff09ce350e.jpeg",
+  "https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/7030/images/10120/franzia-chardonnay__33767.1532452103.1280.1280.jpg?c=2",
+  "https://bremerswineandliquor.com/wp-content/uploads/2019/05/franzia-rich-and-buttery-5l.png",
+  "https://scontent.harristeeter.com/legacy/productimagesroot/DJ/4/61724.jpg",
+  "https://icdn.bottlenose.wine/images/full/501834.png",
+  "https://www.peoriabeerwinespirits.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/M/-/M-1164415.jpg",
+  "https://target.scene7.com/is/image/Target/GUEST_2dbe1341-776e-4677-b392-ea42bc427709?wid=488&hei=488&fmt=pjpeg",
+  "https://cdn2.bigcommerce.com/server5500/tpbc2s65/products/7029/images/10119/franzia-cabernet-sauvignon__01767.1532451847.380.500.jpg?c=2",
+  "https://www.totalwine.com/dynamic/490x/media/sys_master/twmmedia/h3d/h0c/12074159276062.png"
 
 ];
 console.log(random_item(items));
@@ -47,7 +44,7 @@ exports.handler = function(event, context, callback) {
       return client.messages.create({
         from: BOT_NUMBER,
         to: num,
-        body: "Look at that delicious bevvy",
+        body: "It's wine time...",
         mediaUrl: [random_item(items)]
       });
     })
